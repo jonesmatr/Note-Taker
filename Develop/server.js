0fff7dd7491d6797024ca138aaaf410ff1302510
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-01 19:47:09 -0400

    Set up HTML routes for the Express.js server.

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
