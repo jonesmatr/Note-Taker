9171e5e3d40798503cb9415e10581a2a76846971
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-08-01 20:58:47 -0400

    Moved the catch-all route to the bottom of the server file.

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
@@ -60,4 +56,9 @@ app.delete('/api/notes/:id', (req, res) => {
             res.json({ message: 'Note deleted successfully!' });
         });
     });
+
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
